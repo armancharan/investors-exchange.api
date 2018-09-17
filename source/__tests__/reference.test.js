a21444001be1d145bedb67c9085606eef7a66b4c
Arman Charan <armancharan@armancharan.com>
2018-09-17 23:49:10 +1000

    Ref does not exist

--- source/__tests__/reference.test.js
+++ source/__tests__/reference.test.js
@@ -6,6 +6,6 @@ describe('REFERENCE', () => {
   test('Corporate Actions', async () => await reference.corporate_actions())
   test('Dividends', async () => await reference.dividends())
   test('Next Day Ex Date', async () => await reference.next_day_ex_date())
-  test('Symbols', async () => await ref.symbols())
+  test('Symbols', async () => await reference.symbols())
   test('Symbols Directory', async () => await reference.symbol_directory())
 })
