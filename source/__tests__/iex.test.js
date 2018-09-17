e4ef1949900abeaeb9a18024586e206cf94ac7b6
Arman Charan <armancharan@armancharan.com>
2018-09-17 23:48:51 +1000

    Market + Reference are not functions

--- source/__tests__/iex.test.js
+++ source/__tests__/iex.test.js
@@ -6,7 +6,7 @@ import stock from '../stock'
 // IEX.
 describe('IEX', () => {
   it('Exists', () => expect(IEX).toBeTruthy())
-  test('Market', () => expect(IEX.market() instanceof stock).toBeTruthy())
-  test('Reference', () => expect(IEX.reference() instanceof reference).toBeTruthy())
+  test('Market', () => expect(IEX.market instanceof stock).toBeTruthy())
+  test('Reference', () => expect(IEX.reference instanceof reference).toBeTruthy())
   test('Stock', () => expect(IEX.stock() instanceof stock).toBeTruthy())
 })
