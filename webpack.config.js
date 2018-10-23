ddc4394a2958d9f3149f7c4366d6746daa3fd6b7
Arman Charan <armancharan@armancharan.com>
2018-10-23 16:00:13 +1000

    Update to TS loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,15 @@ const webpack = require('webpack')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 module.exports = {
   mode: 'production',
-  entry: {
-    'index': './source/index.ts'
-  },
+  entry: './source/index.ts',
+  mode: 'production',
   output: {
     path: path.resolve(__dirname, 'distribution'),
-    filename: '[name].js',
-    libraryTarget: 'umd',
-    library: 'investors-exchange.api',
-    umdNamedDefine: true
+    filename: '[name].js'
   },
   plugins: [new CleanWebpackPlugin('distribution')],
   resolve: {extensions: ['.ts', '.tsx', '.js']},
   module: {
-    rules: [{test: /\.tsx?$/, loader: 'awesome-typescript-loader', exclude: /node_modules/, query: {declaration: true}}]
+    rules: [{test: /\.tsx?$/, loader: 'ts-loader', exclude: [/node_modules/, /__tests__/]}]
   }
 }
