d129b8e5a25d990ed5d09d3fdccd5e7634c13d61
Arman Charan <armancharan@armancharan.com>
2018-10-23 22:33:34 +1000

    Annotate

--- source/index.ts
+++ source/index.ts
@@ -5,6 +5,7 @@ import {stock as s} from './stock'
 const stock = (symbol?: string) => new s(symbol)
 const market = stock('market')
 
+// IEX.
 const IEX = {fetch, market, reference, stock}
 
 // Export.
